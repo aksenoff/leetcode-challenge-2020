0c6bddc00e051beeb09cff0c49854e1216438505
Aleksei Aksenov <aksenoff.a@gmail.com>
2020-06-24 17:59:38 +0300

    Add june problem 24

---
+++ june-24-unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/unique-binary-search-trees/
+
+class Solution {
+public:
+    int numTrees(int n) {
+        int* catalan = new int[n + 1]{0};
+        catalan[0] = 1;
+        for (int i = 1; i <= n; ++i)
+            for (int j = 0; j < i; ++j)
+                catalan[i] += catalan[j]* catalan[i - j - 1];
+        return catalan[n];
+    }
+};
