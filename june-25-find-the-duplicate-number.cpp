d34f4e952a7fb322e8d3a14598c1708ccfde7901
Aleksei Aksenov <aksenoff.a@gmail.com>
2020-06-25 19:06:40 +0300

    Add june problem 25

---
+++ june-25-find-the-duplicate-number.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+                if (nums[i] == nums[j]) return nums[i];
+        }        
+        return 0;
+    }
+};
